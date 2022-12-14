a05a3131042142073346ff2a2ac0130a3311217c
Ming <ironhuluwa@gmail.com>
2022-12-14 15:27:36 -0800

    problem 3

--- problems/03-make-dog.js
+++ problems/03-make-dog.js
@@ -1,5 +1,25 @@
 // Your code here
 
+class Dog {
+	constructor(name) {
+		this.name = name;
+	}
+
+	static makeJet() {
+		const jet = new Dog('Jet')
+		return jet
+	}
+
+	changeName(newName) {
+		this.name = newName;
+		return this.name
+	}
+
+	speak(word) {
+		return `${this.name} says ${word}`
+	}
+}
+
 
 /*****************************************************************************/
 /***************** DO NOT MODIFY ANYTHING UNDER THIS LINE ********************/
