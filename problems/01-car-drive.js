79d97ebebbc54a1e2843bae5c927f077ec7eae2d
Ming <ironhuluwa@gmail.com>
2022-12-14 14:09:43 -0800

    problem 1 solved

--- problems/01-car-drive.js
+++ problems/01-car-drive.js
@@ -1,4 +1,15 @@
 // Your code here
+class Car {
+	constructor(speed = 0) {
+		this.speed = speed
+	}
+	drive(newSpeed) {
+		newSpeed = this.speed;
+		return newSpeed;
+	}
+}
+
+
 
 /*****************************************************************************/
 /***************** DO NOT MODIFY ANYTHING UNDER THIS LINE ********************/
