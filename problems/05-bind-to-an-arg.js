04ffbbf7f5e51469115cfd17768b7a51aaf27cf7
Ming <ironhuluwa@gmail.com>
2022-12-14 15:47:57 -0800

    problem 5

--- problems/05-bind-to-an-arg.js
+++ problems/05-bind-to-an-arg.js
@@ -1,5 +1,7 @@
 function bindToAnArg(func, arg) {
   // Your code here
+  const answer = func.bind(func, arg)
+  return answer
 }
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
